aae57e39f29ca1010c0c9e0a8a40e3b5730268b2
tahaHelmy1121 <tahahelmy196@gmail.com>
2024-02-10 07:32:03 +0200

    edit

---
+++ lib/features/change_local/change_local.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChangeLocale extends StatefulWidget {
+  const ChangeLocale({super.key});
+
+  @override
+  State<ChangeLocale> createState() => _ChangeLocaleState();
+}
+
+class _ChangeLocaleState extends State<ChangeLocale> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
